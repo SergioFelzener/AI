efc446f225dc2719c54af09bbff4420f17741c53
Sergio Felzener <felzenerdj@gmail.com>
2020-03-03 17:25:15 -0300

    exercicio 19
    finalizando 19

---
+++ TSI2020s/AI/exerciciosTS(typescript)/exe19.js
@@ -0,0 +1,18 @@
+"use strict";
+// Colete 3 números do usuário, coloque-os em um vetor e exiba-o. Em seguida, 
+// modifique o array de forma que a sequência de números fique do contrário 
+// (se digitou 1,2,3, exiba 3,2,1).
+exports.__esModule = true;
+var rs = require("readline-sync");
+var ArrayUser = new Array;
+var ArrayInvertido = new Array;
+function inverteArray() {
+    for (var i = 0; i < 3; i++) {
+        var numeroDigitado = Number(rs.question('Digite um numero'));
+        ArrayUser.push(numeroDigitado);
+    }
+    ArrayInvertido = ArrayUser.slice(0).reverse();
+    console.log(ArrayUser);
+    console.log(ArrayInvertido);
+}
+inverteArray();
---
+++ TSI2020s/AI/exerciciosTS(typescript)/exe19.ts
@@ -0,0 +1,21 @@
+// Colete 3 números do usuário, coloque-os em um vetor e exiba-o. Em seguida, 
+// modifique o array de forma que a sequência de números fique do contrário 
+// (se digitou 1,2,3, exiba 3,2,1).
+
+import * as rs from 'readline-sync';
+
+let ArrayUser: Array<number> = new Array;
+let ArrayInvertido: Array<number> = new Array;
+
+function inverteArray(): void {
+
+    for (let i = 0; i < 3; i++) {
+        let numeroDigitado = Number(rs.question('Digite um numero'));
+        ArrayUser.push(numeroDigitado);
+    }
+    ArrayInvertido = ArrayUser.slice(0).reverse();
+
+    console.log(ArrayUser);
+    console.log(ArrayInvertido);
+}
+inverteArray();
