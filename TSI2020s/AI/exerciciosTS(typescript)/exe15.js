72b142166a0d3e29d576e6352cae2bc06bc9dbef
Sergio Felzener <felzenerdj@gmail.com>
2020-03-07 22:38:04 -0300

    finalizando typescript
    faltou o 20

--- TSI2020s/AI/exerciciosTS(typescript)/exe15.js
+++ TSI2020s/AI/exerciciosTS(typescript)/exe15.js
@@ -1,18 +1,15 @@
-
-function EhPrimo(userInput: number):string {
+function EhPrimo(userInput) {
     var primo = 0;
-
     for (var cont = userInput; cont >= 1; cont--) {
         if (userInput % cont == 0) {
-            primo++
+            primo++;
         }
     }
-
     if (primo == 2) {
         return "O número é primo";
-    } else {
+    }
+    else {
         return "O número não é primo";
     }
 }
-
-console.log(EhPrimo(673)); // esse numero e primo !!!
+console.log(EhPrimo(673));
